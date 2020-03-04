4cbe8f2dd68ff82fbc8d5fd94e9d6c848b08a8bc
Enoch Hsiao <51762139+MusicMaster-Code@users.noreply.github.com>
2020-03-03 21:27:07 -0500

    comment links to template code/API and tidied up code

--- pages/login.js
+++ pages/login.js
@@ -6,6 +6,7 @@ const { Content, Footer } = Layout;
 const { SubMenu } = Menu;
 
 export  default function Login() {
+
       /*TODO:
         Create login page. Use ant design components as much as possible.
         Don't worry about routing or setting up actual working login, just make it look good.
@@ -14,76 +15,81 @@ export  default function Login() {
         Your page will be hosted at localhost:3000/login
     */
 
+    /*https://codesandbox.io/s/zkgqo
+      Template code for login page from antd website, 
+      should help in setting up login authentication
+      API: https://ant.design/components/form/       
+    */
+
     return (
       <Layout style={{ minHeight: '100vh' }}>
 
-            <img src="buildIcon.jpg" title="Build Logo" style={{ margin: '15px auto 15px auto', width: '200px', height: '200px', padding: "0px" }}/>
-      
-            <h1 style={{
-              margin: "10px, auto, 10px, auto", 
-              fontSize: "60px",   
-              color: "#fff",
-              fontFamily: "Arvo",
-              fontWeight: "bold",
-              color: "#222",
-              textShadow: 
-              "4px 4px 0px #f5f5f5, 7px 7px 0px rgba(0, 0, 0, 0.2)",
-              letterSpacing: "0.1em",
-              letterSpacing: "0.1em",
-              textAlign: "center"}}>
-            Build UMass
-            </h1>
+        <img src="buildIcon.jpg" title="Build Logo" style={{ margin: '15px auto 15px auto', width: '200px', height: '200px', padding: "0px" }}/>
+  
+        <h1 style={{
+          margin: "10px, auto, 10px, auto", 
+          fontSize: "60px",   
+          color: "#fff",
+          fontFamily: "Arvo",
+          fontWeight: "bold",
+          color: "#222",
+          textShadow: 
+          "4px 4px 0px #f5f5f5, 7px 7px 0px rgba(0, 0, 0, 0.2)",
+          letterSpacing: "0.1em",
+          letterSpacing: "0.1em",
+          textAlign: "center"}}>
+          Build UMass
+        </h1>
 
-            <Form
-              name="normal_login"
-              style={{ margin: 'auto', marginTop: '5px' }}
+        <Form
+          name="normal_login"
+          style={{ margin: 'auto', marginTop: '5px' }}
+        >
+
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Please input your Username!' }]}
             >
+            <Input 
+              placeholder="Username" 
+              prefix={<Icon type="user" style={{ fontSize: 15 }} />}
+              style={{ width: '300px' }}
+            />
+          </Form.Item>
 
-              <Form.Item
-                name="username"
-                rules={[{ required: true, message: 'Please input your Username!' }]}
-                >
-                <Input 
-                  placeholder="Username" 
-                  prefix={<Icon type="user" style={{ fontSize: 15 }} />}
-                  style={{ width: '300px' }}
-                />
-              </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your Username!' }]}
+            >
+            <Input.Password 
+              placeholder="Password" 
+              prefix={<Icon type="lock" style={{ fontSize: 15 }} />}
+              style={{ width: '300px' }}
+            />
+          </Form.Item>
 
-              <Form.Item
-                name="password"
-                rules={[{ required: true, message: 'Please input your Username!' }]}
-                >
-                <Input.Password 
-                  placeholder="Password" 
-                  prefix={<Icon type="lock" style={{ fontSize: 15 }} />}
-                  style={{ width: '300px' }}
-                />
-              </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked">
+              <Checkbox 
+                style={{float: 'left'}}>
+                Remember me
+              </Checkbox>
+              <a href=""
+                  style = {{float: 'right'}}href="">
+                  Forgot password
+              </a>
+            </Form.Item>
+            <Button 
+              type="primary" 
+              htmlType="submit" 
+              style={{ width: '100px', marginLeft: "100px", background: "blue" }}>
+              Log in
+            </Button>
+          </Form.Item>
 
-              <Form.Item>
-                <Form.Item name="remember" valuePropName="checked">
-                  <Checkbox 
-                    style={{float: 'left'}}>
-                    Remember me
-                  </Checkbox>
-                  <a href=""
-                     style = {{float: 'right'}}href="">
-                     Forgot password
-                  </a>
-                </Form.Item>
-                <Button 
-                  type="primary" 
-                  htmlType="submit" 
-                  style={{ width: '100px', marginLeft: "100px", background: "blue" }}>
-                  Log in
-                </Button>
-              </Form.Item>
-
-          </Form>
+        </Form>
 
       </Layout>
-
-    );
-  }
+        );
+}
 

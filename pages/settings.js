9f5228fdd4397993994819c06fa1906cd484644a
PatrickCleary <46229349+PatrickCleary@users.noreply.github.com>
2020-04-06 11:00:04 -0400

    auto formatted

--- pages/settings.js
+++ pages/settings.js
@@ -6,25 +6,25 @@ import { withAuthSync } from '../utils/auth';
 
 const { Content, Footer } = Layout;
 
-const Settings = (props) =>{
-    return(
-        <Layout style={{ minHeight: '100vh' }}>
-    
+const Settings = (props) => {
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+
+      <Layout>
+        <MyHeader pageName="Settings" />
         <Layout>
-        <MyHeader pageName="Settings"/>
-        <Layout>
-        <SideBar selected={'settings'} />
-        <Layout>
-          <Content style={{ margin: '0 16px' }}>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Settings page.</div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Build Umass</Footer>
-          </Layout>
+          <SideBar selected={'settings'} />
+          <Layout>
+            <Content style={{ margin: '0 16px' }}>
+              <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Settings page.</div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Build Umass</Footer>
           </Layout>
         </Layout>
-    
       </Layout>
-        );
+
+    </Layout>
+  );
 
 }
 export default withAuthSync(Settings);
--- pages/signup.js
+++ pages/signup.js
@@ -1,73 +1,68 @@
-import {  Form, Menu, Input, Layout, Tooltip, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete,} from 'antd';
+import { Form, Menu, Input, Layout, Tooltip, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete, } from 'antd';
 import '../node_modules/antd/dist/antd.css'
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import './signup.css';
 const { Content, Footer } = Layout;
 const { SubMenu } = Menu;
 import cookie from 'js-cookie';
 
-  export  default function Signup() {
+export default function Signup() {
 
-    /*https://codesandbox.io/s/7vlew
-      Source code for sign-up page from antd website, 
-      should help in setting up login authentication
-      API: https://ant.design/components/form/   
-    */
+  const formItemLayout = {
+    labelCol: {
+      sm: { span: 8 },
+    },
+    wrapperCol: {
+      sm: { span: 16 },
+    },
+  };
 
-    const formItemLayout = {
-        labelCol: {
-          sm: { span: 8 },
-        },
-        wrapperCol: {
-          sm: { span: 16 },
-        },
-      };
+  const tailFormItemLayout = {
+    wrapperCol: {
+      xs: {
+        span: 23,
+        offset: 0,
+      },
+      sm: {
+        span: 16,
+        offset: 8,
 
-      const tailFormItemLayout = {
-        wrapperCol: {
-          xs: {
-            span: 23,
-            offset: 0,
-          },
-          sm: {
-            span: 16,
-            offset: 8,
-            
-          },
-        },
-      };
+      },
+    },
+  };
 
-    return (
+  return (
 
-      <Layout className = 'container' style={{ minHeight: '100vh' }}>
-  
-        <h1 style={{
-          marginTop: '5%',
-          marginBottom:'2.5%',
-          fontSize: '60px',   
-          fontFamily: 'Arial, Times, Times New Roman, Arvo',
-          fontWeight: 'bold',
-          color: 'white',
-          textAlign: 'center',
-          textShadow: '2px 0 black',
-          opacity: 1,
-          zIndex: 1,}}>
-          Build UMass
+    <Layout className='container' style={{ minHeight: '100vh' }}>
+
+      <h1 style={{
+        marginTop: '5%',
+        marginBottom: '2.5%',
+        fontSize: '60px',
+        fontFamily: 'Arial, Times, Times New Roman, Arvo',
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
+        textShadow: '2px 0 black',
+        opacity: 1,
+        zIndex: 1,
+      }}>
+        Build UMass
         </h1>
 
-        <Form
-          {...formItemLayout}
-          layout= 'horizontal'
-          className='boxes'
-          style={{position: 'relative', margin: 'auto', marginTop: '0%'}}
-        >
+      <Form
+        {...formItemLayout}
+        layout='horizontal'
+        className='boxes'
+        style={{ position: 'relative', margin: 'auto', marginTop: '0%' }}
+      >
 
-          <Form.Item
-            name='email'
-            label='E-mail'
-            style={{color: 'white', fontSize: '5px'}}
-            rules={[
+        <Form.Item
+          name='email'
+          label='E-mail'
+          style={{ color: 'white', fontSize: '5px' }}
+          rules={[
             {
               type: 'email',
               message: 'The input is not valid E-mail!',
@@ -76,93 +71,93 @@ import cookie from 'js-cookie';
               required: true,
               message: 'Please input your E-mail!',
             },
-            ]}
-          >
-            <Input 
-              placeholder='E-mail' 
-              style = {{width: '270px', border: '1px solid black' }}
-            />
-          </Form.Item>
+          ]}
+        >
+          <Input
+            placeholder='E-mail'
+            style={{ width: '270px', border: '1px solid black' }}
+          />
+        </Form.Item>
 
-          <Form.Item
-              name='password'
-              label='Password'
-              rules={[
-              {
-                  required: true,
-                  message: 'Please input your password!',
+        <Form.Item
+          name='password'
+          label='Password'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password
+            placeholder='Password'
+            style={{ width: '270px', border: '1px solid black' }}
+          />
+        </Form.Item>
+
+        <Form.Item
+          name='confirm'
+          label='Confirm Password'
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: 'Please confirm your password!',
+            },
+            ({ getFieldValue }) => ({
+              validator(rule, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject('The two passwords do not match!');
               },
-              ]}
-              hasFeedback
-          >
-              <Input.Password 
-                placeholder='Password' 
-                style={{ width: '270px', border: '1px solid black' }} 
-              />
-          </Form.Item>
+            }),
+          ]}
+        >
+          <Input.Password
+            placeholder='Confirm Password'
+            style={{ width: '270px', border: '1px solid black' }}
+          />
+        </Form.Item>
 
-          <Form.Item
-              name='confirm'
-              label='Confirm Password'
-              dependencies={['password']}
-              hasFeedback
-              rules={[
-              {
-                  required: true,
-                  message: 'Please confirm your password!',
-              },
-              ({ getFieldValue }) => ({
-                  validator(rule, value) {
-                  if (!value || getFieldValue('password') === value) {
-                      return Promise.resolve();
-                  }
-                  return Promise.reject('The two passwords do not match!');
-                  },
-              }),
-              ]}
-          >
-              <Input.Password 
-                placeholder='Confirm Password' 
-                style={{ width: '270px', border: '1px solid black'}}
-              />
-          </Form.Item>
+        <Form.Item
+          name='phone'
+          label='Phone Number'
+          rules={[{ required: true, message: 'Please input your phone number!' }]}
+          style={{ color: 'white' }}
+        >
+          <Input
+            placeholder='Phone Number'
+            style={{ width: '270px', border: '1px solid black' }}
+          />
+        </Form.Item>
 
-          <Form.Item
-            name='phone'
-            label='Phone Number'
-            rules={[{ required: true, message: 'Please input your phone number!' }]}
-            style={{color: 'white' }}
-          >
-              <Input 
-                placeholder='Phone Number' 
-                style={{ width: '270px', border: '1px solid black'}} 
-              />
-          </Form.Item>
+        <Form.Item
+          name='agreement'
+          {...tailFormItemLayout}
+          className='checkbox'
+        >
+          <Checkbox style={{ color: 'white', fontSize: '1em' }}>
+            I have read the <a href='' style={{ color: 'cyan' }}>agreement</a>
+          </Checkbox>
+        </Form.Item>
 
-          <Form.Item 
-            name='agreement' 
-            {...tailFormItemLayout}
-            className='checkbox'
-            >
-            <Checkbox style={{color: 'white', fontSize: '1em'}}> 
-              I have read the <a href='' style={{color: 'cyan'}}>agreement</a>
-            </Checkbox>
-          </Form.Item>
-
-          <Form.Item {...tailFormItemLayout}>
-            <Button 
-              type='primary' 
-              htmlType='submit'
-              className='button'
-              onClick={()=>cookie.set('session','user',{ expires: 3 })}
-              style={{ width: '100px', background: 'white', color: 'black', border: '1px solid black' }}>                     
-              Register
+        <Form.Item {...tailFormItemLayout}>
+          <Button
+            type='primary'
+            htmlType='submit'
+            className='button'
+            onClick={() => cookie.set('session', 'user', { expires: 3 })}
+            style={{ width: '100px', background: 'white', color: 'black', border: '1px solid black' }}>
+            Register
             </Button>
-          </Form.Item>
+        </Form.Item>
 
-        </Form>
+      </Form>
 
-      </Layout>
+    </Layout>
 
-    );
-  }
+  );
+}

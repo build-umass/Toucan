2740b4476c296b9e1d9b0200c3b10b321c82caa6
PatrickCleary <46229349+PatrickCleary@users.noreply.github.com>
2020-04-06 18:28:45 -0400

    header change

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -1,13 +1,11 @@
-import { Layout, Menu} from 'antd';
 import React, {useState} from 'react';
-const {Header } = Layout;
 import Background from '../public/Randomized-Pattern.svg'
-
+import './componentcss/header.css';
 
 export default function MyHeader(props){
   return(
       <div className = "Title" style = {{backgroundImage: `url(${Background})`, backgroundSize:'cover', display: 'flex', justifyContent:'center', alignItems:'center', width:'100%'}}> 
-        <h1 style = {{fontFamily: 'Montserrat, sans-serif', fontSize: 40, color:'white',margin:'2%'}}color= "white"><strong>{props.pageName}</strong></h1>
+        <h1 style = {{fontFamily: 'Montserrat, sans-serif',  color:'white',margin:'2%'}}color= "white"><strong>{props.pageName}</strong></h1>
       </div>
   );
 }
